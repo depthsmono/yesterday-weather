36823490e3021c518cec3db793dc073b9311bc08
Ed Thompson <ed.thompson@gmail.com>
2025-09-22 15:54:08 -0400

    Initial Commit

---
+++ Yesterday Weather/Yesterday_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yesterday_WeatherApp.swift
+//  Yesterday Weather
+//
+//  Created by Ed Thompson on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Yesterday_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Yesterday WeatherTests/Yesterday_WeatherTests.swift
@@ -0,0 +1,16 @@
+//
+//  Yesterday_WeatherTests.swift
+//  Yesterday WeatherTests
+//
+//  Created by Ed Thompson on 9/22/25.
+//
+
+import Testing
+
+struct Yesterday_WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
